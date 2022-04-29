26782c950275832774d1e7674d4b63eaf94db98e
THALICE SILVA <2020101106@academicougb.com.br>
2022-04-29 14:37:36 -0300

    Finalização Editor html

--- EditorHtml/Viewer.cs
+++ EditorHtml/Viewer.cs
@@ -1,9 +1,12 @@
 using System;
 using System.Text.RegularExpressions;
 
-namespace EditorHtml{
-    public class Viewer{
-        public static void Show(string Text) {
+namespace EditorHtml
+{
+    public class Viewer
+    {
+        public static void Show(string Text)
+        {
             Console.Clear();
             Console.BackgroundColor = ConsoleColor.White;
             Console.ForegroundColor = ConsoleColor.Black;
@@ -13,12 +16,32 @@ namespace EditorHtml{
             Console.ReadKey();
             Menu.Show();
         }
-    
-        public static void Replace(string text){
+
+        public static void Replace(string text)
+        {
             var strong = new Regex(@"<\s*strong[^>]*>(.*?)<\s*/\s*strong>");
             var words = text.Split(' ');
-            for(var i=0; i<=words.length; i++){
-                
+            for (var i = 0; i < words.Length; i++)
+            {
+                if (strong.IsMatch(words[i]))
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.Write(
+                        words[i].Substring(
+                            words[i].IndexOf('>') + 1,
+                            (
+                                (words[i].LastIndexOf('<') - 1) - words[i].IndexOf('>')
+                            )
+                        )
+                    );
+                    Console.Write(" ");
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.Write(words[i]);
+                    Console.Write(" ");
+                }
             }
         }
     }
